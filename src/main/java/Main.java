b799c4ef3911d0e589facfcc9da1afa2532bbe44
Shefat-E-Zannat <shefatezannat@gmail.com>
2020-06-07 16:03:16 +0600

    git initialize

---
+++ src/main/java/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("----------------------------------------------\n");
+        System.out.println("Jenkins basic learning");
+        System.out.println(args[0]);
+        System.out.println(args[1]);
+        System.out.println("----------------------------------------------\n");
+
+    }
+}
